0ec1be6f6a7c9ea758b0d4e351a82d98a2c97d39
Yen <yen0224@icloud.com>
2021-03-14 15:39:34 +0800

    add cpp ex01-11

---
+++ CPPCODE/cpp01.cpp
@@ -0,0 +1,28 @@
+//this is the solution to EX01_11
+//student's ID: S0951037, Chia-Yen Hsu
+//complier platform MacOS 11.2.3 Clang++(apple clang 12.0.0)
+//encode: UTF-8
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    //current population is 312032486
+    long current_amount = 312032486;
+    //every day has 86400 sec.
+    //86400*465=31536000
+    long yearSecound = 31536000;
+    //birth rate per year
+    double birthPerYear = yearSecound / 7;
+    //death rate per year
+    double deathPerYear = yearSecound / 13;
+    //immigrant rate per year
+    double immigrantPerYear = yearSecound / 45;
+    //output the current population
+    cout << "Current population is " << current_amount << endl;
+    //use for loop to output the polulation for the next 5 year
+    for (int i = 1; i < 6; i++)
+    {
+        cout << "After " << i << " Year, the population will be "<< current_amount + int(i * birthPerYear) - int(i * deathPerYear) + int(i * immigrantPerYear) << endl;
+    }
+    return 0;
+}
