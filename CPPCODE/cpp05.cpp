0fd937378d4007fbf68ba3a792ff8d840f35a23a
Yen <yen0224@icloud.com>
2021-03-15 00:51:35 +0800

    add cpp05

---
+++ CPPCODE/cpp05.cpp
@@ -0,0 +1,22 @@
+//solution to ex05-47
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    double userinput[10],sum = 0, devsum = 0,squareSum=0,avg;
+    //double userinput[10] = {1, 2, 3, 4.5, 5.6, 6, 7, 8, 9, 10};
+    for(int i=0;i<10;i++){
+        cin>>userinput[i];
+        sum+=userinput[i];   
+    }
+    avg=sum/10;
+    cout << "The mean is " << avg << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        devsum+=pow(userinput[i]-avg,2);
+    }
+    cout << "The standard deviation is " << sqrt(devsum/9) << endl;
+
+    return 0;
+}
