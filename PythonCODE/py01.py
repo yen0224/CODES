5f22481c5d8877551807afe38060ec8b89c5b0c2
Yen <yen0224@icloud.com>
2021-03-14 16:08:44 +0800

    add 註解

--- PythonCODE/py01.py
+++ PythonCODE/py01.py
@@ -1,3 +1,6 @@
+# this is the solution to EX01_11, written by Chia-Yen, Hsu (S0951037)
+# python version 3.8.5 (conda)
+# coding=UTF-8
 current_amount = 312032486
 yearSecound = 31536000
 print("Current population is", current_amount)
