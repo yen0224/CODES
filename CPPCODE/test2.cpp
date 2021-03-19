4f797dc1fd0953a2943d5a8a7c0ca4443f274b05
Yen <yen0224@icloud.com>
2021-03-19 22:28:13 +0800

    add test2.cpp

---
+++ CPPCODE/test2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void helloworld(){
+    cout<<"helloworld"<<endl;
+}
+int main(int argc, char const *argv[]){ 
+    int people [10];
+    for (int i = 0; i < 10; i++)
+    {
+        helloworld();
+        //cin>> people [i];
+
+    }
+    /*for ( int i = 0; i < 10 ; i++)
+    {
+        cout<< people [i]<<endl;
+    }*/
+    
+    
+    return 0;
+}
