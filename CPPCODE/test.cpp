c8acb4942ba77c81970ba723a44d78c8bc63b55c
Yen <yen0224@icloud.com>
2021-03-22 14:04:36 +0800

    日期錯誤 Fixes #7

--- CPPCODE/test.cpp
+++ CPPCODE/test.cpp
@@ -3,12 +3,7 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     /* code */
-    int k=10;
-    k++;
-    cout<<k++<<endl;
-    cout<<k--<<endl;
-    cout<<++k<<endl;
-    --k;
-    cout<<--k<<endl;
+    cout<<static_cast<double>(1)/2<<endl;
+    cout<<static_cast<double>(1/2)<<endl;
     return 0;
 }
