4c5ccb000115e57c4f1bef1dbfd5dd80396c8415
Yen <yen0224@icloud.com>
2021-03-29 23:20:49 +0800

    修正拼字錯誤

--- HWUPLOAD/py01.py
+++ HWUPLOAD/py01.py
@@ -2,8 +2,8 @@
 # python version 3.8.5 (conda)
 # coding=UTF-8
 current_amount = 312032486
-yearSecound = 31536000
+yearSecond = 31536000
 print("Current population is", current_amount)
 for i in range(1, 6):
     print("After", i, "year, the population will be", current_amount +
-          i*yearSecound//7-i*yearSecound//13+i*yearSecound//45)
+          i*yearSecond//7-i*yearSecond//13+i*yearSecond//45)
