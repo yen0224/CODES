cb30fe09e6d4a53a93dbbf438111c9054c139432
Yen <yen0224@icloud.com>
2021-04-18 15:16:53 +0800

    calculus hw UPLD

--- HWUPLOAD/HW/calculusHW.py
+++ HWUPLOAD/HW/calculusHW.py
@@ -11,30 +11,37 @@ Simpson's 積分法
 """
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy as sp
-N = 1000
-b = np.pi
-a = 0
-delta = (b-a)/N
-sum = 0
-xi = 0
-ans = 0
-X = np.zeros(shape=N+1)
-Y = np.zeros(shape=N+1)
+# N = 1000  # 分割數目
+b = np.pi  # 積分終點值
+a = 0  # 積分初始值
+times = [10, 50, 100, 500, 1000]
+for N in times:
+    delta = (b-a)/N  # deltaX
+    ODDsum = 0
+    EVENsum = 0
+    xi = 0
+    xj = 0
+    ans = 0
+    X = np.zeros(shape=2*N+1)
+    Y = np.zeros(shape=2*N+1)
+    for i in range(1, 2*N, 2):
+        xi = a+i*delta/2
+        ODDsum = ODDsum+np.sin(xi)
+        # print(i)
+        X[i] = xi
+        Y[i] = np.sin(xi)
+    for i in range(2, 2*N, 2):
+        xj = a+i*delta/2
+        EVENsum = EVENsum+np.sin(xj)
+    ans = delta/6*(np.sin(a)+2*EVENsum+4*ODDsum+np.sin(b))  # 積分值 (答案)
+    print(ans)
+"""
+# 繪圖部分
 
-for i in range(1, N):
-    xi = a+i*delta
-    sum = sum+np.sin(xi)
-    print(i)
-    X[i] = xi
-    Y[i] = np.sin(xi)
-
-ans = delta/2.0*(np.sin(a)+2*sum+np.sin(b))  # 積分值 (答案)
 X[0] = a
 Y[0] = np.sin(0)
 X[N] = b
 Y[N] = np.sin(b)
-print(ans)
 
 plt.plot(X, Y)
 plt.xlim(a, b)
@@ -43,3 +50,4 @@ plt.xlabel("phase")
 plt.ylabel("Sine  (arb.)")
 plt.title("Plot  of  Sine  function")
 plt.show()
+"""
